4572ce0974348ae02bd403dbaed4b250c202a76f
Mateus Paulart <mateusp23@rede.ulbra.br>
2023-05-18 17:10:29 -0300

    first commit (aula 1 fazendo front e crud back)

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import {
+  Bai_Jamjuree as BaiJamjuree,
+  // eslint-disable-next-line prettier/prettier
+  Roboto_Flex as Roboto
+} from 'next/font/google'
+import { ReactNode } from 'react'
+import './globals.css'
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const baiJamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
+
+export const metadata = {
+  title: 'NLW Spacetime',
+  description:
+    'Uma cápsula do tempo criada com React, Next.js, Tailwindcss e Typescript.',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
