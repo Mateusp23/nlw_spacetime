c60f64711ee88a7fd1475686a520fdd5b93c55e8
Mateus Paulart <mateusp23@rede.ulbra.br>
2023-05-20 18:49:03 -0300

    personalizando rota inical do app

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,25 +1,13 @@
 import * as SecureStore from 'expo-secure-store'
 import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 
 import { useRouter } from 'expo-router'
 
-import {
-  Roboto_400Regular,
-  // eslint-disable-next-line prettier/prettier
-  Roboto_700Bold, useFonts
-} from '@expo-google-fonts/roboto'
-
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
-import { styled } from 'nativewind'
 import { useEffect } from 'react'
-import blurBg from '../src/assets/bg-blur.png'
 import { api } from '../src/assets/lib/api'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
-import Stripes from '../src/assets/stripes.svg'
-
-const StyledStripes = styled(Stripes)
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
@@ -30,12 +18,6 @@ const discovery = {
 
 export default function App() {
   const router = useRouter()
-
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
 
   const [, response, signInWithGithub] = useAuthRequest(
     {
@@ -75,18 +57,8 @@ export default function App() {
     }
   }, [response])
 
-  if (!hasLoadedFonts) {
-    return null
-  }
-
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className="relative flex-1 items-center bg-gray-900 px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
 
@@ -116,6 +88,6 @@ export default function App() {
       </Text>
 
       <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
   )
 }
